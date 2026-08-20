5660bc9e045d70641c635759c37b683ba68cf6d0
Ibrohimjon Khabibullayev <misterkhabibullayev1@gmail.com>
2026-08-20 15:53:02 +0500

    bottomheader.jsx fayli a tegi to'girlandi

--- src/components/BottomHeaderTop.jsx
+++ src/components/BottomHeaderTop.jsx
@@ -60,7 +60,7 @@ function BottomHeaderTop() {
               href="https://www.google.com/maps/place/IT+Park/@41.5584113,60.6209181,18z/data=!4m6!3m5!1s0x41dfc9842c81fc9d:0x9e434d2b8a616ce9!8m2!3d41.5584081!4d60.6218835!16s%2Fg%2F11s8nxmc2_?entry=ttu&g_ep=EgoyMDI2MDgxMi4wIKXMDSoASAFQAw%3D%3D"
               target="_blank"
               rel="noopener noreferrer"
-              className="max-w-37.5 font-FiraSans font-normal text-[10px] md:text-[14px] leading-[110%] text-black dark:text-white group-hover:text-[#FEC80B] transition-all duration-300"
+              className="max-w-37.5 md:max-w-full font-FiraSans font-normal text-[10px] md:text-[14px] leading-[110%] text-black dark:text-white group-hover:text-[#FEC80B] transition-all duration-300"
             >
               {t("header.address")}
             </a>
