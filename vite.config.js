c2f318f6e354c5be9b2238ddd29550cb0d391a08
Ibrohimjon Khabibullayev <misterkhabibullayev1@gmail.com>
2026-08-23 14:02:25 +0500

    Performance yaxshilandi

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,21 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    babel({ presets: [reactCompilerPreset()] })
+    babel({ presets: [reactCompilerPreset()] }),
+    ViteImageOptimizer({
+      // Barcha papkalardagi rasmlarni qamrab olish
+      includePublic: true,
+      include: ['**/*.{png,jpg,jpeg,gif,svg,webp,avif}'],
+      
+      png: { quality: 80 },
+      jpeg: { quality: 80 },
+      webp: { quality: 80 },
+    }),
   ],
 })
