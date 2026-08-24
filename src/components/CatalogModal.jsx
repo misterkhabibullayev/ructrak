321fd8727d385af658b1fa7a504391d10f3fb610
Ibrohimjon Khabibullayev <misterkhabibullayev1@gmail.com>
2026-08-24 11:18:56 +0500

    catalogmodaldagi overflow to'g'irlandi

--- src/components/CatalogModal.jsx
+++ src/components/CatalogModal.jsx
@@ -70,11 +70,11 @@ function CatalogModal({ activeMenu, onClose }) {
             ease: [0.04, 0.62, 0.23, 0.98],
           }}
           onClick={onClose}
-          className="absolute top-[130%] left-0 z-30 w-full overflow-auto"
+          className="absolute top-[130%] left-0 z-30 w-full overflow-hidden"
         >
           <div
             onClick={(e) => e.stopPropagation()}
-            className="w-full h-screen bg-zinc-200 text-black dark:bg-slate-900 dark:text-white"
+            className="w-full h-screen bg-zinc-200 text-black dark:bg-slate-900 dark:text-white overflow-y-auto"
           >
             <div
               className={`container1 transition-all duration-500 ${
